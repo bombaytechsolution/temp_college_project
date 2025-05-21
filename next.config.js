320948798fa69aff055f6fe8b31043da06b6140b
shubhankar <shubhankar@bombaytechsolutions.com>
2025-05-21 19:58:08 +0530

    Add Amplify configuration, update Next.js settings, and modify package.json

--- next.config.js
+++ next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: { 
+    unoptimized: true,
+    domains: ['*'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  distDir: '.next',
 };
 
 module.exports = nextConfig;
